6fd5f8a5eefd68d72a57bae77c15b89a66b21004
MdShijanAli <freelancershijan@gmail.com>
2023-09-23 20:49:17 +0600

    single note delete setup done

--- src/Pages/SingleNote/SingleNote.jsx
+++ src/Pages/SingleNote/SingleNote.jsx
@@ -1,21 +1,42 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import useTitle from "../../hoocks/useTitle";
 import { BiEdit } from "react-icons/bi";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import toast from "react-hot-toast";
+
 
 const SingleNote = () => {
-  const singleNote = useLoaderData();
-  console.log(singleNote)
-    useTitle(`${singleNote?.title}`)
+  const {title, description, _id} = useLoaderData();
+  // console.log(singleNote)
+  useTitle(`${title}`)
+  const navigate = useNavigate();
+  
+
+  const handleDelete = (_id) => {
+    const url = `http://localhost:5000/note/${_id}`;
+    console.log(url)
+    fetch(url, {
+      method: "DELETE",
+    })
+      .then(() => {
+        toast.success("Note Deleted Successfully");
+        navigate('/')
+      })
+      .catch(err => {
+      toast.error(err)
+    })
+  }
+
+
   return (
     <div>
       <div className="card m-10 bg-base-100 shadow-xl">
   <div className="card-body">
-          <h2 className="card-title">{ singleNote?.title}</h2>
-          <p className=" break-words">{singleNote?.description }</p>
+          <h2 className="card-title">{ title}</h2>
+          <p className=" break-words">{description }</p>
     <div className="card-actions justify-end">
       <button className="bg-green-800 rounded-full p-3"><BiEdit className="w-6 h-6 text-white"></BiEdit></button>
-      <button className="bg-red-700 rounded-full p-3"><RiDeleteBin6Line className="w-6 h-6 text-white"></RiDeleteBin6Line></button>
+      <button onClick={()=>handleDelete(_id)} className="bg-red-700 rounded-full p-3"><RiDeleteBin6Line className="w-6 h-6 text-white"></RiDeleteBin6Line></button>
           </div>
   </div>
 </div>
