843052bec7f90cd0141cc923e23a15559f51f0f1
MdShijanAli <freelancershijan@gmail.com>
2023-09-28 22:09:36 +0600

    responsive issue fixed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -39,37 +39,37 @@ const Sidebar = () => {
                         </div>
                    <div className="text-center text-white">
             <h2 className="text-2xl pt-5 font-semibold">{user? user?.displayName : "No Name" }</h2>
-            <p className="text-xl">{ user? user?.email : "no-email@gmail.com"}</p>
+            <p className="md:text-xl text-md">{ user? user?.email : "no-email@gmail.com"}</p>
                    </div>
 
         </div>
         <hr className="hidden md:block" />
-        <div className="">
-        <ul className="menu p-4 lg:w-100 text-base-content  ">
+        <div className="my-auto">
+                  <div>
+                            <ul className="menu p-4 lg:w-100 text-base-content  ">
+
+                                                    <li className='md:text-xl text-md'>
+                                                    <NavLink className='text-white hover:text-[#00b22d]' to='/'>
+                                                            <BiSolidDashboard ></BiSolidDashboard>
+                                                            <span className='ml-2'> All Notes</span>   
+                                                        </NavLink>
+                                                    </li>
+
+                                                    <li className='md:text-xl text-md my-3'>
+                                                        <NavLink className='text-white hover:text-[#00b22d] ' to='/add-note'>
+                                                            <BiSolidNotepad />
+                                                            <span className='ml-2'> Add Note</span>
+                                                        </NavLink>
+                                                    </li>
 
 
-
-                      <li className='text-xl '>
-                      <NavLink className='text-white hover:text-[#00b22d]' to='/'>
-                                <BiSolidDashboard ></BiSolidDashboard>
-                                <span className='ml-2'> All Notes</span>   
-                          </NavLink>
-                      </li>
-
-                      <li className='text-xl my-3'>
-                          <NavLink className='text-white hover:text-[#00b22d] ' to='/add-note'>
-                                <BiSolidNotepad />
-                                <span className='ml-2'> Add Note</span>
-                          </NavLink>
-                      </li>
-
-
-                      </ul>
+                                    </ul>
+                  </div>
 
 
                       <div className='mx-5'>
 
-                          <button onClick={handleLogout} className='btn bg-[#00b22d] hover:bg-[#00b22d] w-full text-white'>
+                          <button onClick={handleLogout} className='btn btn-md bg-[#00b22d] hover:bg-[#00b22d] w-full text-white'>
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                               </svg>
